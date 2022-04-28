715161900d4f1e99f535a2ea06b802a5cd019a62
Tsovinar1986 <babakhanyan-tsovinar@rambler.ru>
2022-04-28 15:34:32 +0400

    1.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+"""1"""
+# name = input("Please enter your name   ")
+# surname = input("Please enter your surname   ")
+# age = int(input("Please enter your age   "))
+# year = 2022 - age
+# print(f'Hello my name is {name} and and my surname is {surname}. I was born on {year} and my age is {age} years old ')
+
+"""2"""
+#print("""\n\tPython is powerful .... and fast; \n\tplays well with others;\n\truns everywhere;is friendly & easy to learn.""")
